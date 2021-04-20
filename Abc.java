ef9a7f54ea46bc62377d7d1e439fb871b3f7cb91
Shivani Beniwal <shivani.beniwal@ibm.com>
2021-04-20 13:44:52 +0530

    created AtoZ.java and  xyz.java is same and altered abc.java

--- Abc.java
+++ Abc.java
@@ -2,7 +2,7 @@ public class Abc
 {
    public static void main(String args[])
    {
-       System.out.println("from Abc");   
+       System.out.println("Hey! i am from Abc");   
    }
-
+   System.out.println("simply added a statement")
 }
